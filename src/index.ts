5f595eaf3cdb5d5ee817dd0e88245ebf7f9ea829
JoseFredes <jose.fredes@mayor.cl>
2024-12-08 20:39:27 -0300

    Refactor exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export { registerComponent, renderComponent, JSONComponent } from "./core";
+export { renderComponentFromJSON } from "./core";
+export * from "./adapters/react-adapter";
+export * from "./adapters/vue-adapter";
+export * from "./adapters/generic-adapter";

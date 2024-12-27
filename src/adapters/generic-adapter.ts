1fd7d4c08bb2f4c8c0be8c1723e8f09ae332b2df
JoseFredes <jose.fredes@mayor.cl>
2024-12-27 00:31:07 -0300

    Refactor: Update comments in generic-adapter.ts

--- src/adapters/generic-adapter.ts
+++ src/adapters/generic-adapter.ts
@@ -3,7 +3,7 @@ import { renderComponentFromJSON } from "../core";
 /**
  * Generic adapter that returns the generated DOM node..
  * @param json - JSON definition of the component.
- * @returns HTMLElement - Nodo DOM generado.
+ * @returns HTMLElement - DOM node generated.
  */
 export function renderDOMComponent(json: any): HTMLElement {
   return renderComponentFromJSON(json);

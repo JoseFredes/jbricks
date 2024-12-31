d1143cf81438efbfc479cd9925456c091c0b789a
JoseFredes <jose.fredes@mayor.cl>
2024-12-31 19:17:30 -0300

    Refactor renderReactComponent function signature to accept JSONComponent type

--- src/adapters/react-adapter.tsx
+++ src/adapters/react-adapter.tsx
@@ -1,12 +1,12 @@
 import React, { JSX } from "react";
-import { renderComponentFromJSON } from "../core";
+import { renderComponentFromJSON, JSONComponent } from "jbricks";
 
 /**
  * Adapter to render components in React.
  * @param json - JSON definition of the component.
  * @returns JSX.Element - React component.
  */
-export function renderReactComponent(json: any): JSX.Element {
+export function renderReactComponent(json: JSONComponent): JSX.Element {
   const domNode = renderComponentFromJSON(json);
 
   // Returns a React component using `dangerouslySetInnerHTML`.
